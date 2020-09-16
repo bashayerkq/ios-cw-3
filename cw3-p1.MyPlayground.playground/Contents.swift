8884114dbe95c5f96d81ee4784bd8d52a0141254
bashayerkq <70640706+bashayerkq@users.noreply.github.com>
2020-09-16 20:43:06 +0300

    cw3 done

---
+++ cw3-p1.MyPlayground.playground/Contents.swift
@@ -0,0 +1,7 @@
+
+func printMe(){
+    print("my name is bashayer")
+    print("i am 15")
+    print("and my grade was 97 last semester")
+}
+printMe()
---
+++ cw3-p2.MyPlayground.playground/Contents.swift
@@ -0,0 +1,24 @@
+var number1 = Int()
+var number2 = Int()
+func add(number1: Int, number2:Int)->Int{
+    return (number1 + number2)
+}
+let x = add(number1: 4, number2: 6)
+ 
+
+func sub(number1: Int, number2:Int)->Int{
+    return (number1 - number2)
+}
+let y = sub(number1: 4, number2: 6)
+
+
+func mul(number1: Int, number2:Int)->Int{
+    return (number1 * number2)
+}
+let b = mul(number1: 4, number2: 6)
+
+
+func div(number1: Int, number2:Int)->Int{
+    return (number1 / number2)
+}
+let a = div(number1: 4, number2: 6)
